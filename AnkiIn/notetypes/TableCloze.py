8e61bd3d14a7918dbf24a7453bfeeff6fb0d1b1a
Clouder0 <clouder0@outlook.com>
2021-07-28 14:36:02 +0800

    added extra_params to notetypes

--- AnkiIn/notetypes/TableCloze.py
+++ AnkiIn/notetypes/TableCloze.py
@@ -20,11 +20,11 @@ def update_table_cloze_config():
 config_updater.append((update_table_cloze_config, 10))
 
 
-def check(lines: list) -> bool:
+def check(lines: list, extra_params={}) -> bool:
     if len(lines) < 3:
         return False
     return "|" in lines[0] and "|" in lines[1] and "-" in lines[1] and "|" in lines[2]
 
 
-def get(text: str, deck: str = "Export", tags: list = []):
+def get(text: str, deck: str, tags: list, extra_params={}):
     return cget(text=text, deck=deck, tags=tags)

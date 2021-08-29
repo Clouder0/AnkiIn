1f9d181c2360737bba45d7e4dbd3731b9226d2a0
clouder <clouder0@outlook.com>
2021-08-29 12:14:16 +0800

    revert ankiConnectHelper to synchronous

--- AnkiIn/helper/ankiConnectHelper.py
+++ AnkiIn/helper/ankiConnectHelper.py
@@ -1,12 +1,11 @@
+import urllib.request
 import json
 from ..log import helper_logger as log
-import asyncio
-import aiohttp
 
 
-async def check_online():
+def check_online():
     try:
-        await get_deck_names()
+        get_deck_names()
     except Exception:
         log.error("Can't connect to anki-connnect.")
         return False
@@ -17,25 +16,24 @@ def request(action, **params):
     return {"action": action, "params": params, "version": 6}
 
 
-async def invoke(action, **params):
+def invoke(action, **params):
     requestJson = json.dumps(request(action, **params)).encode("utf-8")
-    async with aiohttp.ClientSession() as session:
-        async with session.post("http://localhost:8765", data=requestJson) as resp:
-            response = json.loads(await resp.text())
-            if len(response) != 2:
-                raise Exception("response has an unexpected number of fields")
-            if "error" not in response:
-                raise Exception("response is missing required error field")
-            if "result" not in response:
-                raise Exception("response is missing required result field")
-            if response["error"] is not None:
-                raise Exception(response["error"])
-            return response["result"]
+    response = json.load(urllib.request.urlopen(
+        urllib.request.Request("http://localhost:8765", requestJson)))
+    if len(response) != 2:
+        raise Exception("response has an unexpected number of fields")
+    if "error" not in response:
+        raise Exception("response is missing required error field")
+    if "result" not in response:
+        raise Exception("response is missing required result field")
+    if response["error"] is not None:
+        raise Exception(response["error"])
+    return response["result"]
 
 
-async def add_note(target, options={"allowDuplicate": True}, retry=True):
+def add_note(target, options={"allowDuplicate": True}, retry=True):
     try:
-        return await invoke("addNote", note={
+        return invoke("addNote", note={
             "deckName": target.deck,
             "modelName": target.model.modelName,
             "fields": target.outputfields,
@@ -52,47 +50,48 @@ async def add_note(target, options={"allowDuplicate": True}, retry=True):
                 options:%s\n
                 retry:%s\n""", target.__str__(), target.deck, options.__str__(), retry)
             return
-        if "model" in e.args[0] and target.model.modelName not in (await get_model_names_and_ids()).keys():
+        if "model" in e.args[0] and target.model.modelName not in get_model_names_and_ids().keys():
             log.info("Model %s is not found, creating...",
                      target.model.modelName)
-            await create_model(target.model)
-        elif "deck was not found" in e.args[0] and target.deck not in (await get_deck_names()):
+            create_model(target.model)
+        elif "deck was not found" in e.args[0] and target.deck not in get_deck_names():
             log.info("Deck %s is not found, creating...", target.deck)
-            await create_deck(target.deck)
+            create_deck(target.deck)
         if retry:
-            return await add_note(target, options, False)
+            return add_note(target, options, False)
 
 
-async def create_deck(deckName):
-    return await invoke("createDeck", deck=deckName)
+def create_deck(deckName):
+    return invoke("createDeck", deck=deckName)
 
 
-async def get_model_names_and_ids():
-    return await invoke("modelNamesAndIds")
+def get_model_names_and_ids():
+    return invoke("modelNamesAndIds")
 
 
-async def get_deck_names():
-    return await invoke("deckNames")
+def get_deck_names():
+    return invoke("deckNames")
 
 
-async def create_model(model):
-    return await invoke("createModel",
-                        modelName=model.modelName,
-                        inOrderFields=model.fields,
-                        css=model.css,
-                        isCloze=model.isCloze,
-                        cardTemplates=model.templates
-                        )
+def create_model(model):
+    return invoke("createModel",
+                  modelName=model.modelName,
+                  inOrderFields=model.fields,
+                  css=model.css,
+                  isCloze=model.isCloze,
+                  cardTemplates=model.templates
+                  )
 
 
-async def add_notes(notes, options={"allowDuplicate": True}):
+def add_notes(notes, options={"allowDuplicate": True}):
     for x in notes:
-        await add_note(x, options)
+        add_note(x, options)
 
 
-async def find_notes(query: str):
-    return await invoke("findNotes", query=query)
+def find_notes(query: str):
+    return invoke("findNotes", query=query)
 
 
-async def update_note_fields(id: str, Note):
-    await invoke("updateNoteFields", note={"id": id, "fields": Note.outputfields})
+def update_note_fields(id: str, Note):
+    invoke("updateNoteFields", note={"id": id, "fields": Note.outputfields})
+
